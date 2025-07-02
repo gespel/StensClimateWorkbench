c466f5e2def1fa36b69fd46ac4ea848ac901e1f3
gespel <sten.heimbrodt@hotmail.de>
2025-07-02 16:26:18 +0200

    nice working graphs

--- plotter.py
+++ plotter.py
@@ -1,4 +1,6 @@
 import matplotlib.pyplot as plt
+from matplotlib.pyplot import legend
+
 import crawler
 
 class Plotter:
@@ -8,5 +10,5 @@ class Plotter:
     def plot_year(self, year):
         data = self.crawler.get_historical_temperature(f"{year}-01-01", f"{year}-12-31")
         dates = [i for i in range(0, len(data["hourly"]["time"]))]
-        plt.plot(dates, data["hourly"]["temperature_2m"])
+        plt.plot(dates, data["hourly"]["temperature_2m"], label=year)
         print(f"Plotting done for year {year}")
