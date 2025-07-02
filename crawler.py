3a46387cb925ea917e3aa4fc32f29eb1fccd27c9
sheimbrodt <sheimbrodt@psi.de>
2025-07-02 15:39:58 +0200

    added functionality

--- crawler.py
+++ crawler.py
@@ -1,4 +1,5 @@
 import requests
+import matplotlib.pyplot as plt
 
 class Crawler:
     def __init__(self):
@@ -24,3 +25,16 @@ class Crawler:
         except KeyError:
             print("Fehler beim Auslesen der Temperaturdaten.")
             print(data)
+
+    def get_historical_temperature(self, start, end):
+        response = requests.get(
+            f"https://archive-api.open-meteo.com/v1/era5?latitude=52.52&longitude=13.41&start_date={start}&end_date={end}&hourly=temperature_2m")
+        if response.status_code == 200:
+            return response.json()
+        return None
+
+if __name__ == "__main__":
+    c = Crawler()
+    data = c.get_historical_temperature("2021-01-01", "2021-01-31")
+    plt.plot(data["hourly"]["time"], data["hourly"]["temperature_2m"])
+    plt.show()
