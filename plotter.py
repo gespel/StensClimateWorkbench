4873b16926d4053985b650fcc3b48f367483a3f2
gespel <heimbrodt@uni-potsdam.de>
2025-07-02 23:27:33 +0200

    added pandas and smoothed

--- plotter.py
+++ plotter.py
@@ -1,7 +1,10 @@
 import matplotlib.pyplot as plt
 from matplotlib.pyplot import legend
-
+import numpy as np
+from math import factorial
+import pandas as pd
 import crawler
+
 
 class Plotter:
     def __init__(self, crawler):
@@ -10,5 +13,8 @@ class Plotter:
     def plot_year(self, year):
         data = self.crawler.get_historical_temperature(f"{year}-01-01", f"{year}-12-31")
         dates = [i for i in range(0, len(data["hourly"]["time"]))]
-        plt.plot(dates, data["hourly"]["temperature_2m"], label=year)
+        temps = data["hourly"]["temperature_2m"]
+        df = pd.DataFrame({"date": dates, "temp": temps})
+        df["temp_smooth"] = df["temp"].rolling(window=240, center=True).mean()
+        plt.plot(df["date"], df["temp_smooth"], label=year)
         print(f"Plotting done for year {year}")
